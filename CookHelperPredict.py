df4ce44bb8644508c1d2316667f272128eddc491
VladPodlesnyi <vladpodlesny75@gmail.com>
2021-12-08 01:03:34 +0100

    EDIT 2 CookHelperPredict.py

--- CookHelperPredict.py
+++ CookHelperPredict.py
@@ -2,6 +2,10 @@ from azure.cognitiveservices.vision.customvision.prediction import CustomVisionP
 from msrest.authentication import ApiKeyCredentials
 import os
 
+# Get path to images folder
+dirname = os.path.dirname(__file__)
+images_folder = os.path.join(dirname, 'Test')
+
 # Create variables for your project
 publish_iteration_name = "CookHelperPub"
 project_id = "f4ec7aed-3c70-4698-a41c-bde9ce39b2e5"
@@ -12,11 +16,11 @@ prediction_key = "546ce298309f4867b39e7d18edbde621"
 endpoint = "https://northeurope.api.cognitive.microsoft.com/"
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
 predictor = CustomVisionPredictionClient(endpoint, prediction_credentials)
+# Open an image and make a prediction
+with open(os.path.join(images_folder, "french-macaron-recipe.jpg"), "rb") as image_contents:
+    results = predictor.classify_image(project_id, publish_iteration_name, image_contents.read())
+# Display the results
+for prediction in results.predictions:
+    print(f"{prediction.tag_name}: {prediction.probability * 100 :.2f}%")
 
 
-image_url = "https://sugargeekshow.com/wp-content/uploads/2018/01/french-macaron-recipe.jpg"
-
-
-results = predictor.classify_image_url(project_id,publish_iteration_name,url=image_url)
-
-print(results.predictions[1].tag_name)
